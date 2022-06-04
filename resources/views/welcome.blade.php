be5d51cd3398e802b821dec4c2558817611dbe93
minh <minhhanh.nguyen001@umb.edu>
2022-06-04 19:17:47 -0400

    update Database Seeder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,10 +9,12 @@
         <div class="col-md-6">
             <h2>Create an account & book your appointment</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ab iste iure dolores nam minus doloribus vel possimus modi exercitationem illo, laboriosam amet iusto ad quis harum libero. Minima, veniam.</p>
+            @if(!Auth::check())
             <div class="mt-5">
                 <a href="{{ url('register') }}"><button class="btn btn-success">Register as Patient</button></a>
                 <a href="{{ url('login') }}"><button class="btn btn-secondary">Login</button></a>
             </div>
+            @endif
         </div>
     </div>   <!-- end row -->
  
